dcd92176fc5fe2354fe52041c3666e5e8fba9b50
Lenna <128262375+waxingrose@users.noreply.github.com>
2023-09-17 18:52:12 -0400

    completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function (str) { 
 
+    let newString = '';
+
+    for (let i = str.length; i > 0; i--) {
+        newString += str.slice(i - 1, i);
+    }
+    
+    return newString;
+    
 };
 
 // Do not edit below this line

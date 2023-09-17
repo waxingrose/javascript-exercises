44590e35c54a2415af1502525864fa2c148f7099
Lenna <128262375+waxingrose@users.noreply.github.com>
2023-09-17 18:52:12 -0400

    completed 1-4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
 
-};
+    if (num < 0) {
+        return 'ERROR';
+    } 
+    
+    let greet = '';
+
+    for (let i = 0; i < num; i++) {
+            greet += string;
+    }
+    return greet;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (myArray, ...arg) {
 
-};
+    const result = myArray.filter(filterArgs); // filterArgs() passed as ag 
+
+    function filterArgs(item) { 
+
+        return !arg.includes(item); // returns myArray that is not the items in arg
+
+        }
+
+        return result;
+
+    }
+
+
+    
 
 // Do not edit below this line
 module.exports = removeFromArray;

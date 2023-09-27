fc029f58156dbd3069b7b64b28941172cf1c6d5f
Lenna <128262375+waxingrose@users.noreply.github.com>
2023-09-26 20:53:57 -0400

    complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let result = (temp - 32) * 5 / 9; 
+  let rounded = Math.round(result * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let result = (temp * 9 / 5) + 32
+  let rounded = Math.round(result * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
